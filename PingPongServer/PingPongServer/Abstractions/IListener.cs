ebb26de4a1f7ac8c3d5badf0891c17d7af011eac
Dor Hendel <dor1hendel@gmail.com>
2022-01-19 16:46:42 +0200

    added server abstractions

---
+++ PingPongServer/PingPongServer/Abstractions/IListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongServer.Abstractions
+{
+    public interface IListener
+    {
+        Task Listen(int clientNum);
+        Task Accept();
+    }
+}
