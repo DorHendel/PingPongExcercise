9585ac170ec7ec61051388ee236c3649aa6ecd14
Dor Hendel <dor1hendel@gmail.com>
2022-01-19 17:43:17 +0200

    added SocketServer

---
+++ PingPongServer/PingPongServer/Implamentations/SocketServer.cs
@@ -0,0 +1,45 @@
+﻿using PingPongServer.Abstractions;
+using PingPongServer.Implamentations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongServer
+{
+    public class SocketServer : ServerBase
+    {
+
+        private Socket _socket;
+        private IPEndPoint _iPEndPoint;
+        private int _maxClientNum;
+
+        public override Task<IStreamerIO> Accept()
+        {
+            return new Task<IStreamerIO>(()=> new SocketIO(_socket.Accept()));
+        }
+
+        public override Task Bind()
+        {
+            return new Task(()=> _socket.Bind(_iPEndPoint));
+        }
+
+        public override Task Close()
+        {
+            return new Task(() => 
+            {
+                _socket.Shutdown(SocketShutdown.Both);
+                _socket.Close();
+            }
+            );
+        }
+
+        public override Task Listen()
+        {
+            return new Task(() => _socket.Listen(_maxClientNum));
+        }
+    }
+}
