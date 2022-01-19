c1f868bdb1969c4addf278c0b1b6ebe6b47af80a
Dor Hendel <dor1hendel@gmail.com>
2022-01-19 17:11:49 +0200

    added socketIO

---
+++ PingPongServer/PingPongServer/Implamentations/SocketIO.cs
@@ -0,0 +1,26 @@
+﻿using PingPongServer.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongServer.Implamentations
+{
+    class SocketIO : IStreamerIO
+    {
+        Socket _socket;
+        public string Read()
+        {
+            byte[] buffer = new byte[1024];
+            _socket.Receive(buffer);
+            return Encoding.ASCII.GetString(buffer);
+        }
+
+        public void Write(string message)
+        {
+            _socket.Send(Encoding.ASCII.GetBytes(message));
+        }
+    }
+}
