22b28bd2c19d59543e5c57b7e26a3529ce5b2965
Dor Hendel <dor1hendel@gmail.com>
2022-01-20 12:26:42 +0200

    added Console IO

---
+++ PingPongClient/PingPongClient.UI/IO/Abstractions/IInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongClient.UI.IO.Abstractions
+{
+    public interface IInput
+    {
+        string ReadLine();
+    }
+}
---
+++ PingPongClient/PingPongClient.UI/IO/Implementations/ConsoleReader.cs
@@ -0,0 +1,17 @@
+﻿using PingPongClient.UI.IO.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongClient.UI.IO.Implementations
+{
+    public class ConsoleReader : IInput
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
