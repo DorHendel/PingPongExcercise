5842851945d52d3afd307ad840969497bbea7a55
Dor Hendel <dor1hendel@gmail.com>
2022-01-20 10:12:25 +0200

    added consoleWriter

---
+++ PingPongServer/PingPongServer/UIIO/Abstractions/IOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongServer.UIIO.Abstractions
+{
+    public interface IOutput
+    {
+        void WriteLine(string line);
+    }
+}
