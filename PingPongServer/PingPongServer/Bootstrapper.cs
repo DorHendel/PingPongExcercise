ac55e1e87c03816ce347ada1fc2519e546caeafe
Dor Hendel <dor1hendel@gmail.com>
2022-01-20 10:43:00 +0200

    added Bootstrapper

---
+++ PingPongServer/PingPongServer/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using PingPongServer.Abstractions;
+using PingPongServer.Implamentations;
+using PingPongServer.UIIO.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongServer
+{
+    public class Bootstrapper
+    {
+        public IClientReplier GetClientReplier()
+        {
+            IPAddress iPAddress = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0];
+            var server = new SocketServer(new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp), new IPEndPoint(iPAddress,10000), 10);
+            var clientReplier = new ClientReplier(server, new ConsoleWriter());
+            return clientReplier;
+        }
+    }
+}
