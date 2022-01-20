1d2a53b2125a920d8d9573f09a0d91bf60c46e6b
Dor Hendel <dor1hendel@gmail.com>
2022-01-20 12:58:38 +0200

    added SocketServerCommunicator

--- PingPongClient/PingPongClient.Connection.Core/IServerCommunicator.cs
+++ PingPongClient/PingPongClient.Connection.Core/IServerCommunicator.cs
@@ -5,7 +5,7 @@ namespace PingPongClient.Connection.Core
     public interface IServerCommunicator
     {
         bool TryConnect();
-        byte[] Recieve();
+        byte[] Recieve(byte[] buffer);
         void Send(byte[] bytes);
         void Close();
     }
---
+++ PingPongClient/PingPongConnection/SocketServerCommunicator.cs
@@ -0,0 +1,49 @@
+﻿using PingPongClient.Connection.Core;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PingPongConnection
+{
+    public class SocketServerCommunicator : IServerCommunicator
+    {
+        private Socket _socket;
+        private IPEndPoint iPEndPoint;
+
+        public SocketServerCommunicator(Socket socket, IPEndPoint iPEndPoint)
+        {
+            _socket = socket;
+            this.iPEndPoint = iPEndPoint;
+        }
+
+        public void Close()
+        {
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+        }
+
+        public byte[] Recieve(byte[] buffer)
+        {
+            _socket.Receive(buffer);
+            return buffer;
+        }
+
+        public void Send(byte[] bytes)
+        {
+            _socket.Send(bytes);
+        }
+
+        public bool TryConnect()
+        {
+            try
+            {
+                _socket.Connect(iPEndPoint);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
