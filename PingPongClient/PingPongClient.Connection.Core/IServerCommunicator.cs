400b728a1be1599f2aa8aff00a83ee52454940cc
Dor Hendel <dor1hendel@gmail.com>
2022-01-20 12:00:34 +0200

    added IServerCommunicator

---
+++ PingPongClient/PingPongClient.Connection.Core/IServerCommunicator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PingPongClient.Connection.Core
+{
+    public interface IServerCommunicator
+    {
+        bool TryConnect();
+        byte[] Recieve();
+        void Send(byte[] bytes);
+        void Close();
+    }
+}
