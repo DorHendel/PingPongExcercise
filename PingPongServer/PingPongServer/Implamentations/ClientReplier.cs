77bf7bc73cf1f5a3de168e839477293dfd81e7dc
Dor Hendel <dor1hendel@gmail.com>
2022-01-20 15:12:11 +0200

    changed server side ClientReplier reply function- now i realy Finished part 1

--- PingPongServer/PingPongServer/Implamentations/ClientReplier.cs
+++ PingPongServer/PingPongServer/Implamentations/ClientReplier.cs
@@ -26,21 +26,20 @@ namespace PingPongServer.Implamentations
                 await _server.Listen();
                 IStreamerIO streamer = await _server.Accept();
                 _output.WriteLine("Found connection");
-                StreamerReply(streamer, token);
+                StreamerReply(streamer);
                 await Task.Delay(500);
             }
             await _server.Close();
         }
 
-        private async Task StreamerReply(IStreamerIO streamer, CancellationToken token)
+        private async Task StreamerReply(IStreamerIO streamer)
         {
             try
             {
-                    string message = await streamer.Read();
-                    _output.WriteLine("Recieved message : " + message);
-                    await streamer.Write(message);
-                    _output.WriteLine("Sent back message : " + message);
-                    
+                string message = await streamer.Read();
+                _output.WriteLine("Recieved message : " + message);
+                await streamer.Write(message);
+                _output.WriteLine("Sent back message : " + message);
                 await streamer.Close();
             }
             catch (DisconnectedException)
